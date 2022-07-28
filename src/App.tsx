fc4470a92d46b3d1a2f654f93ebe1428510ae0c4
igor.sobral <igor.ribeiro@pagtel.com.br>
2022-07-27 21:05:20 -0300

    chore: config eslint and resolve linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
-import { DefaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={DefaultTheme}>
-      <Button variant='primary' />
-      <Button variant='success' />
-      <Button variant='danger' />
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
 
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,29 +1,23 @@
-import styled, { css } from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
-const buttonVariables = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
-}
+// const buttonVariables = {
+//   primary: 'purple',
+//   secondary: 'orange',
+//   danger: 'red',
+//   success: 'green',
+// }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    background: ${({ theme, variant }) => theme.colors.primary};
-    color: white;
-    border: 0;
-    border-radius: 5px;
-    margin: 8px;
-    /* ${({ variant }) => {
-        return css`
-            background-color: ${buttonVariables[variant]};
-        `
-    }} */
+  width: 100px;
+  height: 40px;
+  color: white;
+  border: 0;
+  border-radius: 5px;
+  margin: 8px;
 `
