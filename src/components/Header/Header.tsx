b2242179a5f23b086639b4681983bcc6eb1f37f4
igorWebMASTER <igor_1917@hotmail.com>
2022-07-28 23:11:12 -0300

    chore: add default layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HeaderContainer } from './styles'
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <span>Logo</span>
+            <nav>
+                <a href="#">
+                    timer
+                </a>
+                <a href="#">
+                    history
+                </a>
+            </nav>
+        </HeaderContainer>
+    )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,3 @@
-import { Header } from "../components/Header";
 
 export function Home() {
     return (
